c7402069ff8c3ca954f9ba7575be6b8000d945af
Vanessa Lamarque <lamarque.van@gmail.com>
2022-04-26 23:20:03 -0400

    planning UI

--- src/App.js
+++ src/App.js
@@ -4,7 +4,27 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<h1>Family Feud App</h1>
+				{/* 
+				Just a rough draft of how i want the screen to look:
+			
+				Left and Right Top Corners:
+				- Title of team
+				- Avatars of each team
+				- Team points
+				- Space for "XXX" when teams get answers wrong
+
+				Middle top center: Total points of board
+				
+				Middle: 8 slots for answers and points
+
+				Where to put:
+					- Team 1 wins button
+					- Team 2 wins button
+					- Restart round button
+					- Wrong answer buttons (triggers "XXX" for each team)
+					- Next round button
+			
+			 	*/}
 			</header>
 		</div>
 	);
